575f65bad8f87159808d414256dfc0692cc470b5
saw Tarmalar <sawtarmalar@saws-MacBook-Pro.local>
2020-06-14 22:57:37 +0630

    login and registeration

---
+++ Messenger/Resources/Extension.swift
@@ -0,0 +1,36 @@
+//
+//  Extension.swift
+//  Messenger
+//
+//  Created by saw Tarmalar on 14/06/2020.
+//  Copyright © 2020 saw Tarmalar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat{
+        return self.frame.origin.y
+    }
+    
+    public var botton: CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat{
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
